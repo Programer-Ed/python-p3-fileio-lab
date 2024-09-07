5d20a339a614d622d15ee7bafe6e411af65db63d
Edwin <moyiedwin8@gmail.com>
2024-09-07 13:34:07 +0300

    file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', mode='w') as f:
+        f.write(file_content)
+        pass
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', mode='a') as f:
+        f.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt') as f:
+        return f.read()
     pass
